ad0e59efc9712a83119c57fc6c3a47cc087e17c0
Edzhamen <148310484+Edzhamen@users.noreply.github.com>
2023-11-22 08:32:47 +0200

    Create vardnica.py
    Dictionary for Python

---
+++ vardnica.py
@@ -0,0 +1,12 @@
+names = [
+    {
+        "name": "Anna",
+        "class": "10a",
+        "grade": 9
+    },
+    {
+        "name":"Oskars",
+        "class": "10c",
+        "grade": 10
+    }
+]
