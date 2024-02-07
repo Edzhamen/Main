c8be4e75f1697dfc4a2856b786556889015966bf
Edzhamen <148310484+Edzhamen@users.noreply.github.com>
2024-02-07 09:34:38 +0200

    classes.py
    DNS

---
+++ classes.py
@@ -0,0 +1,16 @@
+# DNS
+class Cat:
+    age = 10
+    name = 'Emi'
+
+# Objekti/Instances    
+
+cat1 = Cat()
+cat2 = Cat()
+cat3 = Cat()
+
+Cat.age = 20
+
+print(cat1.age)
+print(cat2.age)
+print(cat3.age)
